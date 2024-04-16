cd671688210fedbf640fe5def674837e1108be89
Hubert Lewicki <166704327+Lewicki-Hubert@users.noreply.github.com>
2024-04-16 14:38:00 +0200

    Commit

---
+++ WebApplication1/WebApplication1/Properties/Controllers/AnimalController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Properties.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnimalController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        IActionResult res = Ok();
+        return Ok();
+    }
+}
--- WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7ad99f501816e76858e54f2700b01799d5007d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec9828f46c3a24b751894ced2052ed53a489c24b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
